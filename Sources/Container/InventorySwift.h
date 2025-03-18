e86c1e053039838ee64ae6f9ec4203cead33233b
Aruna-ios <arunakumari.d@cartrade.com>
2025-03-18 18:01:13 +0530

    first commit

---
+++ Sources/Container/InventorySwift.h
@@ -0,0 +1,18 @@
+//
+//  InventorySwift.h
+//  InventorySwift
+//
+//  Created by Vikram on 26/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InventorySwift.
+FOUNDATION_EXPORT double InventorySwiftVersionNumber;
+
+//! Project version string for InventorySwift.
+FOUNDATION_EXPORT const unsigned char InventorySwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InventorySwift/PublicHeader.h>
+
+
